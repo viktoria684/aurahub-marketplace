361c1a20d17c42e40e410c35642ffbda6ca6a541
viktoria684 <lopasheva.vika@mail.ru>
2026-06-28 05:08:32 +0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
   images: { unoptimized: true },
-  trailingSlash: true,
   allowedDevOrigins: ["*"],
   devIndicators: false,
   poweredByHeader: false,
